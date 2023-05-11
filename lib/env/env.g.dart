247d74279f966f20a802dbdb285066917946e317
Seonki.H <ajsthfldu@outlook.com>
2023-05-11 22:16:04 +0900

    first commit

---
+++ lib/env/env.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const apiKey = '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';
+}
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:dart_openai/openai.dart';
+import 'package:flutter/material.dart';
+
+import 'env/env.dart';
+
+
+void main() {
+  OpenAI.apiKey = Env.apiKey;
+  Stream<OpenAIStreamChatCompletionModel> chatStream = OpenAI.instance.chat.createStream(
+    model: "gpt-3.5-turbo",
+    messages: [
+      const OpenAIChatCompletionChoiceMessageModel(
+        content: "hello",
+        role: OpenAIChatMessageRole.user,
+      )
+    ],
+  );
+
+  chatStream.listen((chatStreamEvent) {
+    print(chatStreamEvent.choices[0].delta.content); // ...
+  });
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
