651df646a44d2315cff5068f4ba9c4bd672d95e0
csh991414 <80662795+csh991414@users.noreply.github.com>
2023-06-13 13:29:58 +0900

    Update env.dart

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -4,6 +4,6 @@ part 'env.g.dart';
 @Envied(path: ".env")
 abstract class Env {
   @EnviedField(varName: 'OPEN_AI_API_KEY')
-  static const apiKey = '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';
+  static const apiKey = 
 }
 
